250e463e26153b41ea0be73e47e7d7a529797640
Gilad <giladb@anyvision.co>
2020-01-02 16:57:45 +0200

    creates event class and main file

---
+++ event.py
@@ -0,0 +1,7 @@
+class Event:
+	def __init__(self, length, min_per_week, max_per_week):
+		self.length = length
+		self.min_per_week = min_per_week
+		self.max_per_week = max_per_week
+	def value():
+		return self.length, self.min_per_week, self.max_per_week
---
+++ main.py
@@ -0,0 +1,6 @@
+from event import Event
+def main():
+	e = Event("1", "3", "5")
+	print(e.value)
+if __name__=='__main__':
+	main()
